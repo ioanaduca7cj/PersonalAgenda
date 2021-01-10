1c7714a360fc08feebc14a205d402492f86476ad
Ioana Duca <ioanaduca@outlook.com>
2021-01-10 18:39:32 +0200

    actualizare fisier

---
+++ PersonalAgenda.Android/Data/AgendaDatabase.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PersonalAgenda.Models;
+using System.Linq;
+using System.Text;
+
+namespace PersonalAgenda.Data
+{
+    class AgendaDatabase
+    {
+        readonly SQLiteAsyncConnection _database;
+        public AgendaDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Agenda>().Wait();
+        }
+        public Task<List<Agenda>> GetNotesAsync()
+        {
+            return _database.Table<Agenda>().ToListAsync();
+        }
+        public Task<Agenda> GetNoteAsync(int id)
+        {
+            return _database.Table<Agenda>()
+            .Where(i => i.ID == id)
+           .FirstOrDefaultAsync();
+        }
+        public Task<int> SaveNoteAsync(Agenda note)
+        {
+            if (note.ID != 0)
+            {
+                return _database.UpdateAsync(note);
+            }
+            else
+            {
+                return _database.InsertAsync(note);
+            }
+        }
+        public Task<int> DeleteNoteAsync(Agenda note)
+        {
+            return _database.DeleteAsync(note);
+        }
+    }
+}
---
+++ PersonalAgenda.Android/Models/Agenda.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersonalAgenda.Droid.Models
+{
+    public class Agenda
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
